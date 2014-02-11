fbdc68d7cf8419fd97536005abc75b1446e60de6
tngyn <timngyn@gmail.com>
2014-02-11 13:47:08 -0800

    Fixed naming conventions

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,5 +1,5 @@
-var data = require('../inventorydata.json');
+var inventoryData = require('../inventorydata.json');
 
 exports.view = function(req, res) {
-	res.render('inventory', data);
+	res.render('inventory', inventoryData);
 }
