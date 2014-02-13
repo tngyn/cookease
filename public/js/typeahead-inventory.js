061d1e3320e53428b9d335606c65e0a4f35b1fa8
tngyn <timngyn@gmail.com>
2014-02-13 13:47:49 -0800

    Added searching data

--- public/js/typeahead-inventory.js
+++ public/js/typeahead-inventory.js
@@ -41,24 +41,88 @@ $(document).ready(function() {
 	  ]
 	});
 
+	var meats = new Bloodhound({
+	  datumTokenizer: function(d) { return Bloodhound.tokenizers.whitespace(d.meat); },
+	  queryTokenizer: Bloodhound.tokenizers.whitespace,
+	  local: [
+	    { meat: 'tri-tip beef'},
+	    { meat: 'chicken breasts'},
+	    { meat: 'salmon'},
+	    { meat: 'halibut'}
+	  ]
+	});
+
+	var dairies = new Bloodhound({
+	  datumTokenizer: function(d) { return Bloodhound.tokenizers.whitespace(d.dairy); },
+	  queryTokenizer: Bloodhound.tokenizers.whitespace,
+	  local: [
+	    { dairy: 'milk'},
+	    { dairy: 'yogurt'},
+	    { dairy: 'provolone cheese'},
+	    { dairy: 'eggs'},
+	    { dairy: 'butter'}
+	  ]
+	});
+
+	var condiments = new Bloodhound({
+	  datumTokenizer: function(d) { return Bloodhound.tokenizers.whitespace(d.condiment); },
+	  queryTokenizer: Bloodhound.tokenizers.whitespace,
+	  local: [
+	    { condiment: 'hoisin sauce'},
+	    { condiment: 'sriracha sauce'},
+	    { condiment: 'soy sauce'},
+	    { condiment: 'vegetable oil'},
+	    { condiment: 'lemon juice'}
+	  ]
+	});
 
 	// initialize the bloodhound suggestion engine
 	grains.initialize();
 	produces.initialize();
+	meats.initialize();
+	dairies.initialize();
+	condiments.initialize();
 	 
 	// instantiate the typeahead UI
-	$('.typeahead').typeahead(null, {
+	$('.typeahead').typeahead({
+		highlight:true
+	},
+	{
 		displayKey: 'grain',
 		source: grains.ttAdapter(),
 		templates: {
 	    	header: '<h4 class="inventory-category-search-ac">Grains</h4>'
 	  	}
-		},
-		{
+	},
+	{
 		displayKey: 'produce',
 		source: produces.ttAdapter(),
 		templates: {
 	    	header: '<h4 class="inventory-category-search-ac">Produce</h4>'
 	  	}
-	});
+	},
+	{
+		displayKey: 'meat',
+		source: meats.ttAdapter(),
+		templates: {
+			header: '<h4 class="inventory-category-search-ac">Meats</h4>'
+		}
+	},
+	{
+		displayKey: 'dairy',
+		source: dairies.ttAdapter(),
+		templates: {
+			header: '<h4 class="inventory-category-search-ac">Dairy</h4>'
+		}
+	},
+	{
+		displayKey: 'condiment',
+		source: condiments.ttAdapter(),
+		templates: {
+			header: '<h4 class="inventory-category-search-ac">Condiments</h4>'
+		}
+	}
+	);
+
+
 });
